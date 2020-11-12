cb7ef43069db0eeab40936067d90270c6c605167
Gustavo Cavalcante Ferreira <gustavo2402.00@gmail.com>
2020-11-12 20:39:55 -0300

    Organização dos códigos

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="estilo.css">
-        <title> Full Stack Eletro</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    </head>
-    <body>
-        <!-- Inicio do menu -->
-       
-        <?php
-            include('menu.html');
-        ?>
-        <!-- fim do menu -->
-        <main class="corpo">
-            <h1>Seja bem vindo(a)!</h1>
-            <p>Aqui em nossa loja, programadores tem desconto!</p>
-        </main>
-        
-            <!-- <br><br><br><br><br>
-            <br><br><br><br><br>
-            <br><br><br><br><br>
-            <br><br><br><br><br> -->
-            
-        <?php
-            include_once('footer.html');
-        ?>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css">
+    <title> Full Stack Eletro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Inicio do menu -->
+
+    <?php
+    include('menu.html');
+    ?>
+    <!-- fim do menu -->
+    <main class="container-fluid">
+        <h1>Seja bem vindo(a)!</h1>
+        <p>Aqui em nossa loja, programadores tem desconto!</p>
+    </main>
+
+    <br><br><br><br><br>
+
+
+    <?php
+    include_once('footer.html');
+    ?>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    </body>
+</body>
+
 </html>
--- produtos.php
+++ produtos.php
@@ -15,27 +15,28 @@ if (!$conn) {
     <link rel="stylesheet" href="estilo.css">
     <script src="js/script.js"></script>
     <title>Produtos - Full Stack Eletro</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 
 <body>
     <!-- Inicio do menu -->
-    <?php
-    include('menu.html');
-    ?>
-    <!-- fim do menu -->
     <header>
+        <?php
+        include('menu.html');
+        ?>
+    </header>
+    <!-- fim do menu -->
+
+
+
+    <div class="container-fluid">
 
         <h1>Produtos</h1>
-    </header>
-    <hr>
 
-    
-    <div class="container-fluid">  
-    
+        <hr>
+
         <ul class="nav flex-column">
-        
+
             <p class="display-4">Categorias</p>
             <li class="nav-item" onclick="exibir_todos()">Todos (12)</li>
             <li class="nav-item" onclick="exibir_categoria('geladeira')">Geladeiras (3)</li>
@@ -44,42 +45,43 @@ if (!$conn) {
             <li class="nav-item" onclick="exibir_categoria('lavaroupas')">Lavadoura de roupas (2)</li>
             <li class="nav-item" onclick="exibir_categoria('lavalouca')">Lava-louças(2)</li>
         </ul>
-    </div>
-    <br />
+
+        <br />
 
 
-    <?php
-    $sql = "select * from produtos";
-    $result = $conn->query($sql);
+        <?php
+        $sql = "select * from produtos";
+        $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        while ($rows = $result->fetch_assoc()) {
+        if ($result->num_rows > 0) {
+            while ($rows = $result->fetch_assoc()) {
 
-    ?>
-        
-            <div class="produtos-blocos" style="display:block" id="<?php echo $rows['categoria']; ?>" >
-                <img src="<?php echo $rows["imagem"]; ?>" width="120px" onmouseover="destaque(this)" onmouseout="imgpadrao(this)">
-                <br>
-                <p class="nomeprod"><?php echo $rows["descricao"]; ?></p>
-                <hr>
-                <p class="preco-antigo">R$ <?php echo $rows["preco"]; ?></p>
-                <p class="preco-atual">R$ <?php echo $rows["preco_venda"]; ?></p><br>
-            </div>
-        
-    <?php
+        ?>
+
+                <div class="produtos-blocos" style="display:block" id="<?php echo $rows['categoria']; ?>">
+                    <img src="<?php echo $rows["imagem"]; ?>" width="120px" onmouseover="destaque(this)" onmouseout="imgpadrao(this)">
+                    <br>
+                    <p class="nomeprod"><?php echo $rows["descricao"]; ?></p>
+                    <hr>
+                    <p class="preco-antigo">R$ <?php echo $rows["preco"]; ?></p>
+                    <p class="preco-atual">R$ <?php echo $rows["preco_venda"]; ?></p><br>
+                </div>
+
+        <?php
+            }
+        } else {
+            echo "Nenhum produto cadastrado!";
         }
-    } else {
-        echo "Nenhum produto cadastrado!";
-    }
-    ?>
+        ?>
+    </div>
 
     <?php
-        include_once('footer.html');
+    include_once('footer.html');
     ?>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
