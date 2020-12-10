652ab96e2ea64fc23f13c2a97d7aaa894f6383ac
Gustavo Cavalcante Ferreira <gustavo2402.00@gmail.com>
2020-12-10 20:34:04 -0300

    Deixando código mais limpo

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,43 +1,33 @@
 const Header = () => {
     return (
         
-            <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-                <a className="navbar-brand ml-3" href="index.html"><img src="imag/fslogo.png" width="110px" /></a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-info">
+                <a className="navbar-brand ml-5" href="index.html">
+                    <img src="imag/fslogo.png" alt="fseletro" width="120px" />
+                </a>
 
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#collapseNavbar">
+                    data-target="#collapseNavbar" aria-controls="collapseNavbar" aria-expanded="false">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
             
-                <div className="collapse navbar-collapse ml-5" id="collapseNavbar">
-                    <ul className="nav">
-                        <li className="nav-item">
+                <div className="collapse navbar-collapse justify-content-end" id="collapseNavbar">
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item mr-5">
                             <a className="nav-link text-white" href="produtos.html">Produtos</a>
                         </li>
-                    </ul>
 
-                </div>
-                <div className="collapse navbar-collapse" id="collapseNavbar">
-                    <ul className="nav">
-                        <li className="nav-item">
+                        <li className="nav-item mr-5">
                             <a className="nav-link text-white" href="lojas.html">Nossas lojas</a>
                         </li>
-                    </ul>
 
-                </div>
-                <div className="collapse navbar-collapse" id="collapseNavbar">
-                    <ul className="nav">
-                        <li className="nav-item">
+                        <li className="nav-item mr-5">
                             <a className="nav-link text-white" href="contato.html">Contato</a>
                         </li>
-                    </ul>
 
-                </div>
-                <div className="collapse navbar-collapse" id="collapseNavbar">
-                    <ul className="nav">
-                        <li className="nav-item">
+                        <li className="nav-item mr-5">
                             <a className="nav-link text-white disabled" href="pedidos.html">Desativado</a>
                         </li>
                     </ul>
